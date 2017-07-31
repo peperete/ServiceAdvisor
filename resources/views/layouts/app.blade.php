e37f6f2c549e24427da6ffe4974b34585b9d3671
Sebastian Perez <sebastian.perez@alliansys.com.ar>
2017-07-31 18:21:35 -0300

    Ajustes menores en layout y ruta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -99,7 +99,7 @@
       <p>Copyright © 2017 Service Advisor S.A. | <a href="#">Términos y Condiciones</a></p>
     </div>
     <div class="col-sm-3">
-      <img src="images/logoadvisor02.png" class="img-responsive" style="height:22px" alt="Image">
+      <img src="{{ asset('images/logoadvisor02.png')}}" class="img-responsive" style="height:22px" alt="Image">
     </div>
 
   </div>
