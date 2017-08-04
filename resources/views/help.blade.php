5c0f6cb4e73c6a8bffd4815c4d230bf65b83b34a
Pablo Mendez <mevesa1901@gmail.com>
2017-08-04 18:47:52 -0300

    agregue clases  de CSS para PF, en estilosProyectos.css
    Gregue nuevas imagenes para vista help.

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -2,8 +2,321 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-          <h1 style="text-align:center">Building...</h1>
+    <div class="container">
+                <p>
+                  <div class="container">
+                      <form>
+                          <div class="row">
+                            <div class="col-sm-3"></div>
+                            <div class="col-sm-6">
+                                <div class="input-group">
+                                  <input type="text" class="form-control" placeholder="Ingresa frase o palabra">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-default" type="submit">
+                                          <i class="glyphicon glyphicon-search"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-3"></div>
+                          </div>
+                      </form>
+                </div>
+              </p>
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicadores -->
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+
+          <div class="item active">
+            <img src="{{ asset('images/carpinteria.png') }}" class="img-responsive" alt="Carpinteria1" width="100%" height="100%">
+            <div class="carousel-caption">
+              <h1 class="textoayuda">
+              ¡ PREGUNTAS  FRECUENTES !
+              </h1>
+
+          </div>
+          </div>
+
+          <div class="item">
+            <img src="{{ asset('images/carpinteria-1.png') }} " alt="Carpinteria2" width="100%" height="1000%">
+            <div class="carousel-caption">
+              <h1 class="textoayuda">
+              ¿EN QUE PUEDO AYUDARTE..?
+              </h1>
+          </div>
+          </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+     </div>
+<!--</div>
+yyyyyy-->
+<p>
+<div class="container">
+      <ul class="nav nav-pills nav-justified">
+        <li class="active servicios"><a data-toggle="pill" href="#home">Lo más consultado</a></li>
+        <li class="servicios"><a data-toggle="pill" href="#menu1">Págos</a></li>
+        <li class="servicios"><a data-toggle="pill" href="#menu2">Servicios</a></li>
+        <li class="servicios"><a data-toggle="pill" href="#menu3">Subscripción</a></li>
+      </ul>
+
+  <div class="tab-content">
+    <div id="home" class="tab-pane fade in active">
+<!--Inicio Preguntas Frecuentes-->
+         <h3 class="iconcenter">
+        <span class="glyphicon glyphicon-globe iconos"><br><p class="iconos">Más Consultados</p></span>
+      </h3>
+
+        <h2 class="textfaq categorias">
+            Preguntas frecuentes (FAQ)
+        </h2>
+        <p class="servicios"><strong>Nota:</strong> Las <strong>respuestas</strong> de las siguientes preguntas lograrás visualizarlas al hacer <strong>Clik </strong>en los Items siguientes.</p>
+        <div class="panel-group" id="accordion">
+        <!--Pregunta 01-->
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse1"><strong>¿Cómo publico mi servicio?</strong></a>
+              </h4>
+            </div>
+            <div id="collapse1" class="panel-collapse collapse">
+              <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <hr>
+              <strong>¿Estas conforme con la respuesta?</strong>
+              <button type="button" class="btn btn-success">Sí</button>
+              <button type="button" class="btn btn-danger">No</button>
+              <hr>
+              </div>
+            </div>
+          </div>
+          <!--Pregunta 02-->
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse2"><strong>¿Cómo solicitar un servicio?</strong></a>
+              </h4>
+            </div>
+            <div id="collapse2" class="panel-collapse collapse">
+              <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <hr>
+              <strong>¿Estas conforme con la respuesta?</strong>
+              <button type="button" class="btn btn-success">Sí</button>
+              <button type="button" class="btn btn-danger">No</button>
+              <hr>
+              </div>
+            </div>
+          </div>
+          <!--Pregunta 03 -->
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse3"><strong>¿Qué medios de pago puedo utilizar?</strong></a>
+              </h4>
+            </div>
+            <div id="collapse3" class="panel-collapse collapse">
+              <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <hr>
+              <strong>¿Estas conforme con la respuesta?</strong>
+              <button type="button" class="btn btn-success">Sí</button>
+              <button type="button" class="btn btn-danger">No</button>
+              <hr>
+              </div>
+            </div>
+          </div>
+
+          <!-- Fin Preguntas-->
+        </div>
+
+<!--Fin Preguntas Frecuentes-->
+
+  </div>
+
+    <div id="menu1" class="tab-pane fade">
+          <h3 class="iconcenter">
+        <span class="glyphicon glyphicon-usd iconos"><br><p class="iconos">Págos</p></span>
+          </h3>
+
+          <!-- Pregunta 04-->
+          <h2 class="textfaq categorias">Preguntas frecuentes (FAQ)</h2>
+          <p class="servicios"><strong>Nota:</strong> Las <strong>respuestas</strong> de las siguientes preguntas lograrás visualizarlas al hacer <strong>Clik </strong>en los Items siguientes.</p>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse4"><strong>¿Con qué tarjetas puedo realizar el págo?</strong></a>
+              </h4>
+            </div>
+            <div id="collapse4" class="panel-collapse collapse">
+              <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <hr>
+              <strong>¿Estas conforme con la respuesta?</strong>
+              <button type="button" class="btn btn-success">Sí</button>
+              <button type="button" class="btn btn-danger">No</button>
+              <hr>
+              </div>
+            </div>
+          </div>
+      <!-- Pregunta 05-->
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse5"><strong>¿Qué sucede, si el pago con mi tarjeta es rechazada?</strong></a>
+              </h4>
+            </div>
+            <div id="collapse5" class="panel-collapse collapse">
+              <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <hr>
+              <strong>¿Estas conforme con la respuesta?</strong>
+              <button type="button" class="btn btn-success">Sí</button>
+              <button type="button" class="btn btn-danger">No</button>
+              <hr>
+              </div>
+            </div>
+          </div>
+    </div>
+
+    <div id="menu2" class="tab-pane fade">
+          <!-- Pregunta 06-->
+          <h3 class="iconcenter">
+        <span class="glyphicon glyphicon-wrench iconos"><br><p class="iconos">Servicios</p></span>
+      </h3>
+
+          <h2 class="textfaq categorias">Preguntas frecuentes (FAQ)</h2>
+          <p class="servicios"><strong>Nota:</strong> Las <strong>respuestas</strong> de las siguientes preguntas lograrás visualizarlas al hacer <strong>Clik </strong>en los Items siguientes.</p>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse6"><strong>¿Cómo solicitar un Servicio?</strong></a>
+              </h4>
+            </div>
+            <div id="collapse6" class="panel-collapse collapse">
+              <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <hr>
+              <strong>¿Estas conforme con la respuesta?</strong>
+              <button type="button" class="btn btn-success">Sí</button>
+              <button type="button" class="btn btn-danger">No</button>
+              <hr>
+              </div>
+            </div>
+          </div>
+     <!-- Pregunta 07-->
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse7"><strong>¿Por qué debo suscribirme?</strong></a>
+              </h4>
+            </div>
+            <div id="collapse7" class="panel-collapse collapse">
+              <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <hr>
+              <strong>¿Estas conforme con la respuesta?</strong>
+              <button type="button" class="btn btn-success">Sí</button>
+              <button type="button" class="btn btn-danger">No</button>
+              <hr>
+              </div>
+            </div>
+          </div>
+
+
+
+    </div>
+    <div id="menu3" class="tab-pane fade">
+          <!-- Pregunta 08-->
+          <h3 class="iconcenter">
+        <span class="glyphicon glyphicon-book iconos"><br><p class="iconos">Suscripción</p></span>
+      </h3>
+
+          <h2 class="textfaq categorias">Preguntas frecuentes (FAQ)</h2>
+          <p class="servicios"><strong>Nota:</strong> Las <strong>respuestas</strong> de las siguientes preguntas lograrás visualizarlas al hacer <strong>Clik </strong>en los Items siguientes.</p>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse8"><strong>¿Qué requisitos necesito para suscribirme?</strong></a>
+              </h4>
+            </div>
+            <div id="collapse8" class="panel-collapse collapse">
+              <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <hr>
+              <strong>¿Estas conforme con la respuesta?</strong>
+              <button type="button" class="btn btn-success">Sí</button>
+              <button type="button" class="btn btn-danger">No</button>
+              <hr>
+              </div>
+            </div>
+          </div>
+          <!-- Pregunta 09-->
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse9"><strong>¿Cuáles son los requisitos para suscribirme como Profesional?</strong></a>
+              </h4>
+            </div>
+            <div id="collapse9" class="panel-collapse collapse">
+              <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <hr>
+              <strong>¿Estas conforme con la respuesta?</strong>
+              <button type="button" class="btn btn-success">Sí</button>
+              <button type="button" class="btn btn-danger">No</button>
+              <hr>
+              </div>
+            </div>
+          </div>
+       <!-- Pregunta 10-->
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapse10"><strong>¿Cómo me registro para ofrecer mi servicio?</strong></a>
+              </h4>
+            </div>
+            <div id="collapse10" class="panel-collapse collapse">
+              <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              <hr>
+              <strong>¿Estas conforme con la respuesta?</strong>
+              <button type="button" class="btn btn-success">Sí</button>
+              <button type="button" class="btn btn-danger">No</button>
+              <hr>
+              </div>
+            </div>
+          </div>
+          <!-- Fin Preguntas-->
+    </div>
+  </div>
+</div>
+</p>
     </div>
 
 </div>
