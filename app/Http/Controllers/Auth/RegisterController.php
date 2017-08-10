402e978b8bc4a0bb1c686c38039b08b881b3a00a
Daisy <daisyperez09@gmail.com>
2017-08-10 19:47:53 -0300

    Actualizacion campo foto

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
             'cellphone' => 'required|integer|min:10',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
-            'photo' => 'max:255',
+            'photo' => 'image|max:255',
 
         ]);
     }
