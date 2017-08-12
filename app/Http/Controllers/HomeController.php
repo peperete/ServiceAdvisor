254f9001cb20250e0b6c9bc6e0610487fb64ff21
Sebastian Perez <sebastian.perez@alliansys.com.ar>
2017-08-12 16:33:12 -0300

    ajustar controller para help

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,6 +26,11 @@ class HomeController extends Controller
         return view('home');
     }
 
+    public function help()
+    {
+        return view('help');
+    }
+
 
     public function userRegister()
     {
